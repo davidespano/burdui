0a6a7595ab77f339ae575185bed5ac65b026ef25
Emanuele Concas <conema@outlook.com>
2020-03-19 17:07:35 +0100

    TextField fix

--- examples/js/textField.js
+++ examples/js/textField.js
@@ -4,31 +4,34 @@ window.onload = function(){
 
     // creating the view tree
     let root = new burdui.StackPanel;
-    root.setBounds(new burdui.Bounds(0, 0, 1024, 500))
+    root.setBounds(new burdui.Bounds(0, 0, 1024, 768))
         .setStyle("vertical")
         .setPadding(10);
 
-    let textField1 = new burdui.TextField(undefined, "Text1");
+    let textField1 = new burdui.TextField;
     textField1.setBounds(new burdui.Bounds(100, 100, 250, 75))
         .setBorderColor("#004d00")
         .setBorderLineWidth(1)
         .setFont("25px Arial")
-        .setTextColor("#004d00");
+        .setTextColor("#004d00")
+        .setText("Text1");
 
-    let textField2 = new burdui.TextField(undefined, "Text2");
+    let textField2 = new burdui.TextField;
     textField2.setBounds(new burdui.Bounds(100, 100, 250, 75))
         .setBorderColor("black")
         .setBorderLineWidth(1)
         .setFont("25px Arial")
-        .setTextColor("black");
+        .setTextColor("black")
+        .setText("Text2");
 
-    let textField3 = new burdui.TextField(undefined, "Text3");
+    let textField3 = new burdui.TextField;
     textField3.setBounds(new burdui.Bounds(100, 100, 250, 75))
         .setBorderColor("#eeee00")
         .setBackgroundColor("red")
         .setBorderLineWidth(2)
         .setFont("25px Arial")
-        .setTextColor("white");
+        .setTextColor("white")
+        .setText("Text3");
 
     // adding the views as children of the root panel
     root.addChild(textField1)
