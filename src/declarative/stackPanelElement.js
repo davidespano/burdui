755ef550a8f77f789aef3bf9f30f0b46a11be810
frensis <bacci.francesca@live.it>
2020-03-20 09:22:09 +0100

    Declarative StackPanel

--- src/declarative/stackPanelElement.js
+++ src/declarative/stackPanelElement.js
@@ -1,7 +1,48 @@
 import {ViewElement} from "./viewElement";
+import {StackPanel} from "../views/stackPanel";
 
 class StackPanelElement extends ViewElement{
 
+    constructor() {
+        super();
+        this.buiView = new StackPanel();
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        for(let attr of this.attributes){
+            switch (attr.name) {
+                case 'style':
+                    this.style = attr.value;
+                    break;
+
+                case 'padding':
+                    this.padding = attr.value;
+                    break;
+            }
+        }
+    }
+
+    set style(val){
+        if(val){
+            this.buiView.setStyle(val);
+        }
+    }
+
+    get style(){
+        return this.buiView.getStyle();
+    }
+
+    set padding(val){
+        if(val){
+            this.buiView.setPadding(Number(val));
+        }
+    }
+
+    get padding(){
+        return this.buiView.getPadding();
+    }
 }
 window.customElements.define('bui-stack-panel', StackPanelElement);
 
