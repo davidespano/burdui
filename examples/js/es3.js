e9041f6a4080844772378983a5e5f48a37764f94
DavidPrada8 <108024514+DavidPrada8@users.noreply.github.com>
2022-06-23 12:50:23 +0200

    Delete examples directory

--- examples/js/es3.js
+++
@@ -1,5 +0,0 @@
-window.onload = function(){
-    const screen = document.getElementById("screen");
-    const app = new burdui.App(screen, document.getElementById("root").buiView);
-    app.start();
-};
